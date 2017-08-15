a9713ea12f033a2d8f153128efd75f1c9b85e4a6
Kevin Kellar <kellar.kevin@gmail.com>
2017-08-15 13:55:19 -0700

    broke the quats

--- LighthouseTracking.cpp
+++ LighthouseTracking.cpp
@@ -284,7 +284,7 @@ HmdVector3_t LighthouseTracking::GetPosition(HmdMatrix34_t matrix)
 
 	return vector;
 }
-
+long lastPRCall = 0;
 HmdQuaternion_t LighthouseTracking::GetRotation(HmdMatrix34_t matrix, bool b)
 {
 	HmdQuaternion_t q;
@@ -296,8 +296,11 @@ HmdQuaternion_t LighthouseTracking::GetRotation(HmdMatrix34_t matrix, bool b)
 	q.x = copysign(q.x, matrix.m[2][1] - matrix.m[1][2]);
 	q.y = copysign(q.y, matrix.m[0][2] - matrix.m[2][0]);
 	q.z = copysign(q.z, matrix.m[1][0] - matrix.m[0][1]);
-	if (b)
+	if (b && cpMillis() > lastPRCall + 10)
+	{
 		ProcessRotation(q);
+		lastPRCall = cpMillis();
+	}
 	return q;
 }
 
--- mirror-coords.py
+++ mirror-coords.py
@@ -2,47 +2,47 @@ from vpython import *
 import time
 import threading
 
-#scene.autoscale = False
 
-print("\n")
+hmd = vector(1,0,0)
 
-hmd = vector(0,0,0)
-left = vector(0,0,0)
-right = vector(0,0,0)
-
-devices = [hmd, left, right]
+w = 0;
+oldw = 0;
 
 def process():
-	xyz = ['x','y','z']
-	global devices	
+	print("??")
 	while True:
+		n = 0
 		try:
 			editIndex = -1;
 			nInput = input()
-			splits = nInput.split()    		
+			if not nInput.startswith("PROCESSED"):
+				continue
+			#print("good " + nInput)
+			splits = nInput.split()
 		except Exception as e:
-			break
+			#print("bad " + nInput)
+			print(e)
+			continue
 		else:
 			pass
 		finally:
 			pass
-		for split in splits:
-			if "HMD" in split:
-				editIndex = 0;
-			if "LEFT:" in split:
-				editIndex = 1;
-			if "RIGHT:" in split:
-				editIndex = 2;
 
-			if editIndex < 0 or len(split) < 3 or split[1] != ':' or split[0] not in xyz:
+		for split in splits:
+			if n == 0:
+				n =1
 				continue
 			num = float(split[2:])
-			if split[0] == 'x':
-				devices[editIndex].z = -num;
+			if split[0] == 'w':
+				pass
+			elif split[0] == 'x':
+				hmd.x = num;
 			elif split[0] == 'y':
-				devices[editIndex].y  = num;
+				hmd.y  = num;
 			elif split[0] == 'z':
-				devices[editIndex].x  = -num;
+				hmd.z  = num;
+
+		#print("x:%.3f y:%.3f :%.3f" % (hmd.x,hmd.y,hmd.z))
 
 
 		
@@ -51,16 +51,8 @@ thread.start()
 
 b_hmd = box(color=color.magenta)
 b_hmd.pos = vector(-1,0,0)
-b_left = box(color=color.blue)
-b_left.pos = vector(1,0,0)
-b_right = box(color=color.red)
-b_right.pos = vector(3,0,0)
 
 b_box = box(color=color.white)
 b_box.pos = vector(-3,0,0)
 while True:
-	b_hmd.axis = norm( devices[0] * 2)
-	b_left.axis = norm( devices[1] * 2)
-	b_right.axis = norm( devices[2] * 2)
-
-
+	b_hmd.axis = norm(hmd)
